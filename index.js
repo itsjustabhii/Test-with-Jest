5dc206657f29807eac4083a5bb9504455327884a
itsjustabhii <itsjustabhii@gmail.com>
2023-02-04 15:27:32 +0530

    added new testcases

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const add = (a, b) => {
   return a + b;
 };
 
+const err = () => {
+  throw new Error("I am new Error");
+};
+
+const promiseTest = () => {
+  return new Promise((resolve, reject) => {
+    if (a - b > 0) {
+      resolve("+ve");
+    } else {
+      reject("-ve");
+    }
+  });
+};
+
 module.exports = {
   add,
+  err,
+  promiseTest,
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-const { add } = require("../index");
+const { add, err, promiseTest } = require("../index");
 
 test("toBe", () => {
   expect(add(1, 2)).toBe(3);
@@ -28,4 +28,25 @@ test("toBeCloseTo", () => {
 
 test("toMatch", () => {
   expect(add("Hello", "World")).toMatch(/Hello/);
+});
+
+test("toThrow", () => {
+  expect(() => err()).toThrow("I'm a New Error");
+});
+
+test("promiseTest", () => {
+  promiseTest(1, 2)
+    .then((data) => {
+      expect(data).toBe("+ve");
+    })
+    .catch((e) => {
+      expect(e).toBe("-ve");
+    });
+});
+
+test("promiseTest", () => {
+  expect(prommiseTest(2, 1)).resolves.toBe("+ve");
+});
+test("promiseTest", () => {
+  expect(prommiseTest(1, 2)).rejects.toBe("-ve");
 });
